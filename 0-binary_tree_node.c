57c8c68e0a90d6af62bbcf98d3622e15ca88da12
Khawlaowl <khawlahilli20045@gmail.com>
2024-01-31 14:09:18 -0800

    f

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,20 +3,14 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    // Allocate memory for the new node
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-    
-    // Check if memory allocation was successful
-    if (new_node == NULL)
-        return NULL;
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	return NULL;
 
-    // Initialize the new node
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
-    // Return the pointer to the new node
-    return new_node;
+	return new_node;
 }
-
