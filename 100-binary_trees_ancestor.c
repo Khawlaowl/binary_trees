828353b4cf2500c0a25bb0703847a0c31c9e844f
Notyasser <showhad55@gmail.com>
2024-02-01 15:14:34 +0100

    fix task

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,65 +1,26 @@
 #include "binary_trees.h"
 
 /**
- * findROOT - finds the root of a binary tree
- * @tree: pointer to the tree
- * Return: the root
- */
-const binary_tree_t *findROOT(const binary_tree_t *tree)
-{
-	const binary_tree_t *tmp = tree;
-
-	while (tmp->parent)
-	{
-		tmp = tmp->parent;
-	}
-	return (tmp);
-}
-
-/**
- * findLCA - help function to find the LCA
- * @root: root of the tree
- * @fst: first node
- * @snd: second node
- * Return: pointer to the ancestor
- */
-const binary_tree_t *findLCA(const binary_tree_t *root,
-const binary_tree_t *fst,
-const binary_tree_t *snd)
-{
-	const binary_tree_t *left_lca, *right_lca;
-
-	if (!root)
-		return (NULL);
-
-	if (root == fst || root == snd)
-		return (root);
-
-	left_lca = findLCA(root->left, fst, snd);
-	right_lca = findLCA(root->right, fst, snd);
-
-	if (left_lca && right_lca)
-		return (root);
-	else if (left_lca)
-		return (left_lca);
-	else
-		return (right_lca);
-}
-
-/**
- * binary_trees_ancestor - finds the lowest common ancestor of two nodes
- * @first: pointer to the first node
- * @second: pointer to the second node
- * Return: a pointer to the lowest common ancestor node
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
+ *
+ * Return: If no common ancestors return NULL, else return common ancestor.
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-const binary_tree_t *second)
+		const binary_tree_t *second)
 {
-	const binary_tree_t *root;
+	binary_tree_t *mom, *pop;
 
 	if (!first || !second)
 		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-	root = findROOT(first);
-	return ((binary_tree_t *)findLCA(root, first, second));
+	mom = first->parent, pop = second->parent;
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
+	else if (mom == second || !pop || (!pop->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, pop));
 }
